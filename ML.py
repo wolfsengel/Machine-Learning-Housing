da199aedb8f656cff4e420a97e6f365cc26e9eb6
angelgp <a22angelgp@iessanclemente.net>
2023-05-11 14:03:38 +0200

    Updated

--- ML.py
+++ ML.py
@@ -1,28 +1,26 @@
-# Importar librerías
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-# Cargar conjunto de datos
-from sklearn.datasets import load_boston
-boston = load_boston()
+data_url = "http://lib.stat.cmu.edu/datasets/boston"
+raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO']
 
-# Crear un dataframe con los datos
-df = pd.DataFrame(boston.data, columns=boston.feature_names)
-df['target'] = boston.target
+# cargar el conjunto de datos
+raw_df.columns = column_names
+# definir variables
+X = raw_df['RM'].values.reshape(-1, 1) # variable independiente
+y = raw_df['MEDV'].values.reshape(-1, 1) # variable dependiente
 
-# Definir variables
-X = df['RM'].values.reshape(-1,1)  # variable independiente
-y = df['target'].values.reshape(-1,1)  # variable dependiente
 
-# Crear modelo de regresión lineal
+# crear modelo de regresión lineal
 reg = LinearRegression().fit(X, y)
 
-# Realizar predicciones
+# realizar predicciones
 y_pred = reg.predict(X)
 
-# Graficar resultados
+# graficar resultados
 plt.scatter(X, y, color='black')
 plt.plot(X, y_pred, color='blue', linewidth=3)
 plt.title('Relación entre el tamaño de una casa y su precio de venta')
